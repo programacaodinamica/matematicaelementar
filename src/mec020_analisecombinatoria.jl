2e65170953db7bb1acc3119e00b21effb37117e3
Hallison Paz <hallison.dapaz@gmail.com>
2021-09-03 15:09:28 -0300

    antes da aula de combinatoria

---
+++ src/mec020_analisecombinatoria.jl
@@ -0,0 +1,71 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ de75b3ae-0cdb-11ec-39b2-0b18f53daf36
+md"""
+# Análise Combinatória
+
+## Vídeo da aula
+
+Você pode acessar, gratuitamente, a gravação da última semana aqui neste site. Se você acha este trabalho relevante, [considere apoiar o nosso canal como membro](https://youtube.com/join) e tenha acesso a *todas* as gravações das aulas ao vivo nas categorias **ESPRESSO** e **CAPUCCINO**. 
+
+###### ▶️ [Gravação da aula de 3 de setembro de 2021](https://youtu.be/UYYmBiZdo4M)
+
+"""
+
+# ╔═╡ 9cb5d172-6330-480e-b67c-cafb6625d5d3
+
+
+# ╔═╡ 76ca6d51-469e-4f43-8603-92687d2aeb1a
+# representação de números inteiros
+typemin(Int32)
+
+# ╔═╡ a532231b-0635-4f1f-b224-b29c9cdeab51
+md"""
+
+#### Arranjo com repetição
+
+Seja M um conjunto com m elementos, isto é, M = {a1, a2, ..., am}. Chamamos arranjo com repetição dos m elementos, tomados r a r, toda r-upla ordenada (sequência de tamanho r) formada com elementos de M não necessariamente distintos.
+
+#### Arranjo
+
+Seja M um conjunto com m elementos, isto é, M = {a1, a2, ..., am}. Chamamos de arranjo dos m elementos tomados r a r (1 ⩽ r ⩽ m) a qualquer r-upla (sequência de r elementos) formada com elementos de M, todos distintos.
+
+#### Permutação
+
+Seja M um conjunto com m elementos, isto é, M = {a1, a2, ..., am}. Chamamos de
+permutação dos m elementos a todo arranjo em que r = m.
+
+#### Combinação
+
+Seja $M$ um conjunto com m elementos, isto é, $M = {a_1, a_2, ..., a_m}$. Chamamos de combinações dos $m$ elementos, tomados $r$ a $r$, aos subconjuntos de $M$ constituídos de r elementos.
+"""
+
+# ╔═╡ 9dd2a13c-462f-48fd-8c76-5caf80774f67
+md"""
+
+###### Quantas cores é possível representar com 24 bits?
+"""
+
+# ╔═╡ b07ff640-3bb3-40b1-9f10-199b09edbf0b
+md"""
+
+###### Quantos divisores positivos o número 23456 tem?
+"""
+
+# ╔═╡ ff498eb4-b943-4629-ad49-a8a6622ca573
+md"""
+###### De quantas formas diferentes podemos jogar na Mega Sena?
+"""
+
+# ╔═╡ Cell order:
+# ╟─de75b3ae-0cdb-11ec-39b2-0b18f53daf36
+# ╠═9cb5d172-6330-480e-b67c-cafb6625d5d3
+# ╟─76ca6d51-469e-4f43-8603-92687d2aeb1a
+# ╟─a532231b-0635-4f1f-b224-b29c9cdeab51
+# ╟─9dd2a13c-462f-48fd-8c76-5caf80774f67
+# ╟─b07ff640-3bb3-40b1-9f10-199b09edbf0b
+# ╟─ff498eb4-b943-4629-ad49-a8a6622ca573
